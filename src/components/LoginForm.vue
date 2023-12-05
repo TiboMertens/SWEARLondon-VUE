8362f45a85dc2ffaeb95f5511768a39413c34277
TiboMertens <tibomertens25@gmail.com>
2023-12-05 16:39:52 +0100

    feat: add update password page frontend

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+    <!-- create a form with mail & password input and a submit button -->
+    <form>
+        <div class="mb-[32px]">
+            <div class="mb-4">
+                <label for="email" class="block text-gray-600 text-sm font-semibold mb-2">Email</label>
+                <input type="email" id="email"
+                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#69FF47]" />
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-600 text-sm font-semibold mb-2">Password</label>
+                <input type="password" id="password"
+                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#69FF47]" />
+            </div>
+        </div>
+        <button type="submit"
+            class="w-full bg-[#69FF47] text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-green-500">
+            Update Password
+        </button>
+    </form>
+</template>
+
+<style scoped></style>
---
+++ src/components/UpdatePassword.vue
@@ -0,0 +1,23 @@
+<script setup>
+import LoginForm from './LoginForm.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-col md:flex-row bg-[#dddddd]" style="height: calc(100vh + 28px)">
+        <div class="md:w-1/2 hidden md:block">
+            <img src="../assets/shoes-login.webp" alt="SWEAR brand showcase">
+        </div>
+        <div class="w-full md:w-1/2">
+            <div class="h-screen flex justify-center items-center">
+                <div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-md w-[350px] mx-auto">
+                    <h1 class="font-bold text-2xl text-center mb-6">Update password</h1>
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<style scoped></style>
+  

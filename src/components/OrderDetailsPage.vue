95d2ff9291275ed52c9262db989760b618636e81
TiboMertens <tibomertens25@gmail.com>
2023-12-06 16:24:04 +0100

    feat: get all data from the order

--- src/components/OrderDetailsPage.vue
+++ src/components/OrderDetailsPage.vue
@@ -1,8 +1,45 @@
 <script setup>
+import { useRoute } from "vue-router";
+
+import { ref, onMounted } from 'vue';
+
+const route = useRoute();
+const orderId = route.params.id;
+
+const order = ref({});
+
+const fetchOrder = async () => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/v1/shoes/${orderId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            order.value = result.data;
+            console.log(result.data);
+        } else {
+            console.error(result.message);
+        }
+    } catch (error) {
+        console.error('Error fetching shoe:', error);
+    }
+};
+
+onMounted(() => {
+    fetchOrder();
+});
 </script>
 
 <template>
-    DETAILS
+    <div>
+        <p>Status: {{ order.status }}</p>
+        <!-- Other properties can be displayed similarly -->
+    </div>
 </template>
 
 <style scoped></style>

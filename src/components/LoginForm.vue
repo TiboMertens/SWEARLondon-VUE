52638b694248e3b1c0be3b42f88d94d865449110
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-12-11 21:04:32 +0100

    Feat: Login page setup

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+
+const logIn = async () => {
+    
+}
+
+
+</script>
+
+<template>
+    <form @submit.prevent="logIn">
+        <div class="mb-[32px]">
+            <div class="mb-4">
+                <label for="email" class="block text-gray-600 text-sm font-semibold mb-2">Email</label>
+                <input v-model="email" type="email" id="email"
+                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#69FF47]" />
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-600 text-sm font-semibold mb-2">Password</label>
+                <input v-model="password" type="password" id="password"
+                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#69FF47]" />
+                    <router-link to="/updatePassword">Forgot password?</router-link>
+            </div>
+        </div>
+        <div v-if="errorMessage" class="text-red-500 mb-4 text-xs">{{ errorMessage }}</div>
+        
+        <button type="submit"
+            class="w-full bg-[#69FF47] text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-green-500">
+            Log in
+        </button>
+    </form>
+</template>
+
+<style scoped></style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,8 +1,21 @@
 <script setup>
+import LoginForm from '../components/LoginForm.vue';
 </script>
 
 <template>
-    <router-link to="/updatePassword">Forgot password?</router-link>
+    <div class="flex flex-col md:flex-row bg-[#f7f7f7]" style="height: calc(100vh + 28px)">
+        <div class="md:w-1/2 hidden md:block">
+            <img src="../assets/loginImg.jpg" class="h-screen" alt="SWEAR brand showcase">
+        </div>
+        <div class="w-full md:w-1/2">
+            <div class="h-screen flex justify-center items-center">
+                <div class="flex flex-col items-center p-6 bg-white rounded-lg shadow-md w-[350px] mx-auto">
+                    <h1 class="font-bold text-2xl text-center mb-6">Log in</h1>
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>

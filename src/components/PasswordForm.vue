4077b7aacfa300177305e6787248063025150106
TiboMertens <tibomertens25@gmail.com>
2023-12-12 14:55:42 +0100

    feat: add admin check & old password form

--- src/components/PasswordForm.vue
+++ src/components/PasswordForm.vue
@@ -1,12 +1,46 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { jwtDecode } from "jwt-decode";
 
 const router = useRouter();
 
-const email = ref('');
-const password = ref('');
+const oldPassword = ref('');
+const newPassword = ref('');
 const errorMessage = ref('');
+
+const token = localStorage.getItem('token');
+
+let isAdmin = false;
+
+onMounted(() => {
+    // Check if the user is logged in as an admin
+    const decodedToken = checkAdminStatus();
+});
+
+const checkAdminStatus = () => {
+    if (token) {
+        // Decode the token
+        const decodedToken = jwtDecode(token);
+
+        // Check if the user is an admin
+        if (decodedToken.admin) {
+            isAdmin = true;
+            console.log('User is an admin');
+        } else {
+            isAdmin = false;
+            console.log('User is not an admin');
+        }
+
+        return decodedToken;
+    } else {
+        // User is not logged in
+        isAdmin = false;
+        //redirect to login page
+        router.push('/');
+        console.log('User is not logged in');
+    }
+};
 
 const updatePassword = async () => {
     try {
@@ -47,13 +81,13 @@ const updatePassword = async () => {
     <form @submit.prevent="updatePassword">
         <div class="mb-[32px]">
             <div class="mb-4">
-                <label for="email" class="block text-gray-600 text-sm font-semibold mb-2">Email</label>
-                <input v-model="email" type="email" id="email"
+                <label for="oldPassword" class="block text-gray-600 text-sm font-semibold mb-2">Old password</label>
+                <input v-model="oldPassword" type="oldPassword" id="oldPassword"
                     class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#69FF47]" />
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-gray-600 text-sm font-semibold mb-2">Password</label>
-                <input v-model="password" type="password" id="password"
+                <label for="newPassword" class="block text-gray-600 text-sm font-semibold mb-2">New password</label>
+                <input v-model="newPassword" type="newPassword" id="newPassword"
                     class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-[#69FF47]" />
             </div>
         </div>

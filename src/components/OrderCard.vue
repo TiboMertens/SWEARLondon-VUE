e61ae55a8afab8828b59c1cee09362c7fdb7bf53
TiboMertens <tibomertens25@gmail.com>
2023-12-18 14:19:35 +0100

    feat: add orderID

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -9,8 +9,8 @@ const shoe = ref(props.shoe);
 <template>
     <div class="w-[274px] bg-white rounded-lg shadow-md">
         <div class="p-5">
+            <p class="text-md pb-2"><span class="font-bold">OrderID:</span> {{ shoe.id }}</p>
             <p class="text-md pb-2"><span class="font-bold">User:</span> {{ shoe.username }}</p>
-            <p class="text-md "><span class="font-bold">Order status:</span> {{ shoe.status }}</p>
         </div>
     </div>
 </template>
